2b44e754ac95c8fb3198008773136aa4a6c567c9
Timothy Danford <tdanford@gmail.com>
2014-05-13 16:53:00 -0400

    Trait framework for row group indices, and initial implementation of range index.

---
+++ adam-core/src/main/scala/org/bdgenomics/adam/parquet_reimpl/index/RowGroupRangeIndex.scala
@@ -0,0 +1,53 @@
+/**
+ * Copyright 2014 Genome Bridge LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.bdgenomics.adam.parquet_reimpl.index
+
+import org.bdgenomics.adam.rdd.{ ParquetRowGroup, Footer }
+import org.bdgenomics.adam.models.ReferenceRegion
+import java.io.File
+import scala.io.Source
+
+class RowGroupRangeIndex(val indexFile: File) extends RowGroupIndex[RowGroupRangeIndexEntry] {
+
+  val entries: Seq[RowGroupRangeIndexEntry] =
+    Source.fromFile(indexFile).getLines().map {
+      case line: String => {
+        val array = line.split("\t")
+        val path = array(0)
+        val index = array(1).toInt
+        val ranges: Seq[ReferenceRegion] = array(2).split(",").map(RowGroupRangeIndex.parseRegion).toSeq
+        new RowGroupRangeIndexEntry(path, index, ranges)
+      }
+    }.toSeq
+
+  override def findIndexEntries(predicate: IndexEntryPredicate[RowGroupRangeIndexEntry]): Iterable[RowGroupRangeIndexEntry] = {
+    entries.filter(predicate.accepts)
+  }
+}
+
+object RowGroupRangeIndex {
+  private val referenceRegionRegex = "([^:]+):(\\d+)-(\\d+)".r
+  private def parseRegion(regionString: String): ReferenceRegion = {
+    referenceRegionRegex.findFirstMatchIn(regionString) match {
+      case Some(m) => ReferenceRegion(m.group(1), m.group(2).toLong, m.group(3).toLong)
+      case None    => throw new IllegalArgumentException("\"%s\" doesn't match reference region regex".format(regionString))
+    }
+  }
+}
+
+class RowGroupRangeIndexEntry(path: String, rowGroupIndex: Int, val ranges: Seq[ReferenceRegion])
+    extends RowGroupIndexEntry(path, rowGroupIndex) {
+}
---
+++ adam-core/src/test/scala/org/bdgenomics/adam/parquet_reimpl/index/RowGroupRangeIndexSuite.scala
@@ -0,0 +1,32 @@
+/**
+ * Copyright 2014 Genome Bridge LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.bdgenomics.adam.parquet_reimpl.index
+
+import org.scalatest.FunSuite
+import java.io.File
+
+class RowGroupRangeIndexSuite extends FunSuite {
+
+  test("Can read simple test range index") {
+    val filename = getClass().getClassLoader.getResource("test_rowgroup_rangeindex.1.txt").getFile
+    val file = new File(filename)
+
+    val rangeIndex = new RowGroupRangeIndex(file)
+
+    assert(rangeIndex.entries.size === 1)
+    assert(rangeIndex.entries.head.ranges.size === 2)
+  }
+}
